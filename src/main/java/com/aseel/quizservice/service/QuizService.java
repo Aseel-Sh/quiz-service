0f7c3db478118cca20d899fafdae90a819fa6244
Aseel-Sh <aseel.sh52@gmail.com>
2025-08-06 21:05:04 -0500

    Added rest of methods

--- src/main/java/com/aseel/quizservice/service/QuizService.java
+++ src/main/java/com/aseel/quizservice/service/QuizService.java
@@ -39,29 +39,16 @@ public class QuizService {
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
 
-//        Optional<Quiz> quiz = quizDAO.findById(id);
-//        List<Question> questionsFromDB = quiz.get().getQuestions();
-        List<QuestionWrapper> questionsForUser = new ArrayList<>();
-//        for (Question question : questionsFromDB) {
-//            QuestionWrapper qw = new QuestionWrapper(question.getId(), question.getQuestionTitle(), question.getOption1(), question.getOption2(), question.getOption3(), question.getOption4());
-//            questionsForUser.add(qw);
-//        }
+        Quiz quiz = quizDAO.findById(id).get();
 
+        List<Integer> questionIds = quiz.getQuestionIds();
+        ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
 
-        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+        return questions;
     }
 
     public ResponseEntity<Integer> gradeQuiz(Integer id, List<Response> responses) {
-//        Quiz quiz = quizDAO.findById(id).get();
-//        List<Question> questionsFromDB = quiz.getQuestions();
-        int correctAnswers = 0;
-//        int i = 0;
-//        for (Response response : responses){
-//            if(response.getResponse().equals(questionsFromDB.get(i).getRightAnswer())){
-//                correctAnswers++;
-//                i++;
-//            }
-//        }
-        return new ResponseEntity<>(correctAnswers, HttpStatus.OK);
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+        return score;
     }
 }
